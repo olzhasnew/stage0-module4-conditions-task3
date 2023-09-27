ea2068a17239edcc42c6cf5e8e6da00644d2fd55
olzhasnew <olzhas.ualikhanov@gmail.com>
2023-09-27 23:55:04 +0600

    testing 1st time

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character == 'a' || character == 'e' ||character == 'i' || character == 'o' || character == 'u' || character == 'A' || character == 'E' ||character == 'I' || character == 'O' || character == 'U') {
+            System.out.println("Vowel");
+        } else if ((character>='a'&&character<='z')||(character>='A'&&character<='Z')) {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
